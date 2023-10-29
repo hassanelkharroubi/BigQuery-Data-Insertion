dd113f82ed6294739266d95b1aa45de144b742aa
Hassan EL KHARROUBI <elkharroubihassan@outlook.com>
2023-10-29 12:48:55 +0100

    add new function to handle logger

--- code/big_query.py
+++ code/big_query.py
@@ -1,21 +1,35 @@
 import os
 import json
+import sys
 import requests
 from google.cloud import bigquery
 from google.cloud.exceptions import NotFound
 from google.cloud.exceptions import Conflict
 import time
 import logging
+from logger.handlers import TimedRotatingFileHandler
+from logging import handlers
 from dotenv import load_dotenv
 
-logging.basicConfig(filename='../logs/bigquery_execution.log', 
-                    encoding='utf-8',
-                      level=logging.INFO,
-                      format='%(asctime)s - %(levelname)s - %(message)s',
-                      datefmt='%Y-%m-%d %H:%M:%S'
-                      )
+
+log_directory=sys.argv[1]
+log_filename = "extract_ingest_bigquery_"
+def setup_logger():
+    logger = logger.getLogger(__name__)
+    logger.setLevel(logger.INFO)
+    file_handler = TimedRotatingFileHandler(os.path.join(log_directory, log_filename), when='midnight',
+                                             interval=1, backupCount=0, encoding='utf-8', delay=False)
+    file_handler.suffix = "%Y%m%d.log"
+    file_handler.setLevel(logger.DEBUG)
+    formatter = logger.Formatter('%(asctime)s - %(levelname)s - %(message)s',
+                                  datefmt='%Y-%m-%d %H:%M:%S')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    return logger
+
+logger=setup_logger()
 # load env variables
-logging.info("Import ENV variables")
+logger.info("Import ENV variables")
 load_dotenv()
 grant_type= os.getenv("grant_type")
 client_id=os.getenv("client_id")
@@ -65,7 +79,7 @@ def get_api():
     return base_url,headers
 def get_data(filters):
     data_to_insert=[]
-    logging.info('Getting API CREED ...')
+    logger.info('Getting API CREED ...')
     base_url,headers=get_api()
 
     for filter_type in filters:
@@ -84,56 +98,56 @@ def get_data(filters):
                     vkecu_id = attributes["vkecuId"]
                     log_type = attributes["type"]
                 except KeyError as e:
-                    logging.error(f"KeyError: {e}")
+                    logger.error(f"KeyError: {e}")
                 parameters = attributes.get("parameters", {})
                 parameters=str(parameters)
                 row = (creation_date, collection_date, producer, vkecu_id, log_type,parameters)
                 data_to_insert.append(row)
         except requests.exceptions.RequestException as e:
-            logging.error("Request Exception {} .".format(e))
+            logger.error("Request Exception {} .".format(e))
         except ValueError as e:
-            logging.error("JSON Parsing Exception {} .".format(e))
+            logger.error("JSON Parsing Exception {} .".format(e))
 
         except Exception as e:
-            logging.error("An unexpected exception occurred {} .".format(e))
-    logging.info('Total of records retreived : {}'.format(len(data_to_insert))) 
+            logger.error("An unexpected exception occurred {} .".format(e))
+    logger.info('Total of records retreived : {}'.format(len(data_to_insert))) 
     return data_to_insert
 def delete_create_table(client,table_ref,schema,wait_timeout=300):
-    logging.info("Attempt to delete the table if it exists.")
+    logger.info("Attempt to delete the table if it exists.")
     try:
         client.delete_table(table_ref)
-        logging.info(f"Table '{table_id}' deleted.")
+        logger.info(f"Table '{table_id}' deleted.")
     except NotFound:
-        logging.error(f"Table '{table_id}' does not exist, so it wasn't deleted.")
+        logger.error(f"Table '{table_id}' does not exist, so it wasn't deleted.")
 
     try:
-        logging.info("Creating the new table.")
+        logger.info("Creating the new table.")
         table = bigquery.Table(table_ref, schema=schema)
         table = client.create_table(table)
-        logging.info(f"Table '{table_id}' created.")
+        logger.info(f"Table '{table_id}' created.")
     except Conflict as e:
-        logging.error("Table {} can't not be created du to the conflict - {} ".format(table_id,e))
+        logger.error("Table {} can't not be created du to the conflict - {} ".format(table_id,e))
     return table
 
 def insertion_available(table_ref,client):
-    logging.info("Start time for retreiving table and insertion.")
+    logger.info("Start time for retreiving table and insertion.")
     # for now we will work with while loop until we find the avg time of insertion and availabilty of table 
     start_time = time.time()
     while True:
         try:
             client.get_table(table_ref)
-            logging.info("Table is available.")
+            logger.info("Table is available.")
             return True  # The table is available, exit the loop
         except NotFound:
             if time.time() - start_time >= wait_timeout:
-                logging.error("Table did not become available within the timeout of {}.".format(wait_timeout))
+                logger.error("Table did not become available within the timeout of {}.".format(wait_timeout))
                 return False
-            logging.warning("Table not available yet. Waiting for {} seconds...".format(5))
+            logger.warning("Table not available yet. Waiting for {} seconds...".format(5))
             time.sleep(5)
 
 def insert_data(table,client,data,wait_timeout=300):
     if not table.schema:
-        logging.error("Table schema is empty. Aborting data insertion.")
+        logger.error("Table schema is empty. Aborting data insertion.")
         return False
     else:
         start_time = time.time()
@@ -141,29 +155,29 @@ def insert_data(table,client,data,wait_timeout=300):
         while True:
             try:
                 errors = client.insert_rows(table, data)
-                logging.info("Data has been inserted successfully.")
+                logger.info("Data has been inserted successfully.")
                 break
             except NotFound:
                 if time.time() - start_time >= wait_timeout:
-                    logging.error("The insertion did not become available within the timeout {} seconds.".format(wait_timeout))
+                    logger.error("The insertion did not become available within the timeout {} seconds.".format(wait_timeout))
                     return False
-            logging.warning("Insertion not available yet. Waiting  for {} seconds...".format(5))
+            logger.warning("Insertion not available yet. Waiting  for {} seconds...".format(5))
             i+=1
             time.sleep(5) 
-        logging.info( "The vailability of insertion takes  {} seconds.".format(str(i*5)))
+        logger.info( "The vailability of insertion takes  {} seconds.".format(str(i*5)))
         if not errors:
-            logging.info("Data inserted successfully.")
+            logger.info("Data inserted successfully.")
             end = time.time()
-            logging.info("Total of Time consummed to insert Data successfully is {} seconds .".format(str(end-start_time)))
+            logger.info("Total of Time consummed to insert Data successfully is {} seconds .".format(str(end-start_time)))
             return True
         else:
-            logging.error("Error inserting data: {}".format(errors))
+            logger.error("Error inserting data: {}".format(errors))
             end = time.time()
-            logging.info("Total of Time consummed to insert Data unsuccessfully is {} seconds .".format(str(end-start_time)))
+            logger.info("Total of Time consummed to insert Data unsuccessfully is {} seconds .".format(str(end-start_time)))
             return False
 
 if __name__=="__main__":
-    logging.info("\n---------------Start---------------\n")
+    logger.info("\n---------------Start---------------\n")
     client = bigquery.Client(project=project_id)
     table_ref = client.dataset(dataset_id).table(table_id)
 
